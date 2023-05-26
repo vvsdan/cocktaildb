5eda18fead53dfc3ab7c8504b672f31ab8d162b6
vvsdan <vvsdan999@gmail.com>
2023-05-26 15:13:03 -0400

    inital commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi unde
+        consectetur quo odio dignissimos dolor fuga maxime, accusamus quae magni
+        deserunt, explicabo, quas dolore eum reprehenderit ad facilis ducimus
+        debitis at dolorum? Accusantium dolorem suscipit debitis quidem facilis
+        amet asperiores.
+      </p>
+    </section>
+  );
+};
+
+export default About;
